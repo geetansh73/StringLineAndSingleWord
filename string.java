47f4bda4768cc1ef6a8b8590d2fb34dc7ae75784
Geetansh Gupta <67698473+geetansh73@users.noreply.github.com>
2021-02-24 14:17:07 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class axax
+{
+public static void main(String g[])
+{
+    Scanner sc=new Scanner(System.in);
+String s="axax";
+System.out.println(s);
+String b=sc.nextLine();//read onlu two words of string in whole line
+System.out.println(b);
+}}
